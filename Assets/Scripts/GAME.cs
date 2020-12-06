19f69a58e3b253837c6817eef1f7411f33ab10a1
CiberTurtle <58660466+CiberTurtle@users.noreply.github.com>
2020-12-05 19:06:05 -0600

    Initial commit

---
+++ Assets/Scripts/GAME.cs
@@ -0,0 +1,20 @@
+﻿#pragma warning disable 649
+using UnityEngine;
+
+public class GAME : MonoBehaviour
+{
+	// Singleton
+	static GAME m_current;
+	public static GAME current { get => m_current; }
+
+	void Awake()
+	{
+		if (m_current != null)
+			Destroy(gameObject);
+
+		m_current = this;
+		DontDestroyOnLoad(gameObject);
+
+		transform.SendMessage("Ready", SendMessageOptions.DontRequireReceiver);
+	}
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# RE-LIVE
+ The repo for the new Post-LD Game: RE:LIVE
